524658e284c40a8fffb578e90d4bfc7b26773cf0
ArunMandal <marun1406@gmail.com>
2024-02-17 15:02:46 -0600

    Add project files.

---
+++ TokenAuth.API/Data/ApplicationDbConnect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TokenAuth.API.Data
+{
+    public class ApplicationDbConnect:DbContext
+    {
+        public ApplicationDbConnect():base("DefaultConnection")
+        {
+
+        }
+
+
+    }
+}
